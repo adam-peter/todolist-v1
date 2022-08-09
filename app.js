5ab7218173cc5f84f06d0a372e0ff9e9974039f8
Adam Petržela <90480113+badmood111@users.noreply.github.com>
2022-08-09 14:21:32 +0200

    Add styling, add work route

--- app.js
+++ app.js
@@ -6,37 +6,58 @@ const secret = require("./secret.json")
 //setup
 const app = express()
 const port = 3000
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json());
 app.set('view engine', 'ejs')
-app.use(express.static(__dirname+"/public"))
+app.use(express.static(__dirname + "/public"))
 
 //variables
-let items = ["Buy food", "Cook food", "Eat food"]
+let days = ["Buy food", "Cook food", "Eat food"]
+let workItems = []
 
-//get
-app.get("/", (req, res)=>{
+//get:
+app.get("/", (req, res) => {
   const options = {
     weekday: "long",
     month: "long",
     day: "numeric"
   }
   const day = new Date().toLocaleDateString("en-US", options)
-  console.log(day);
   res.render("list", {
-      day: day,
-      items: items
+    listTitle: day,
+    items: days
   })
 })
 
-app.post("/", (req, res)=>{
-  items.push(req.body.item)
-  res.redirect("/")
+app.get("/work", (req, res)=>{
+  res.render("list", {
+    listTitle: "Work list",
+    items: workItems
+  })
+})
+
+//post:
+app.post("/", (req, res) => {
+  let item = req.body.item
+  if (req.body.list === "Work"){
+    workItems.push(item)
+    res.redirect("/work")
+  } else{
+    days.push(item)
+    res.redirect("/")
+  }
+})
+
+app.post("/work", (req, res)=>{
+  workItems.push(req.body.item)
+  res.redirect("/work")
 })
 
 
 
 
-app.listen(port, ()=>{
+app.listen(port, () => {
   console.log("Server runnin on port 3000!");
 })
